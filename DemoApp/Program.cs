2e0b0185ea11bbbb559482532e3879639d9df841
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-09-11 18:08:52 +0200

    initial commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        public static int Sum(int a, int b){
+            return a+b;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
